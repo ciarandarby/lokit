0bb00ada108d38c0ea6ee43a816bbb5577aa3f87
Ciarán Darby <100690391+ciarandarby@users.noreply.github.com>
2026-05-29 17:19:08 +0100

    fix: Windows backslash path replacements for C binaries

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
 import glob
+
+from setuptools import setup
 
 try:
     from mypyc.build import mypycify
-    
+
     src_files = glob.glob("src/lokit/**/*.py", recursive=True)
-    src_files = [f for f in src_files if "importers.py" not in f]
-    
+    src_files = [f.replace("\\", "/") for f in src_files if "importers.py" not in f]
+
     ext_modules = mypycify(
         src_files,
         opt_level="3",
