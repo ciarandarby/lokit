c1744ce99be97607f2b709d23a11112e0c63c10d
Ciarán Darby <100690391+ciarandarby@users.noreply.github.com>
2026-05-29 17:29:31 +0100

    fix: Pure Python Packages for Windows Build (temp resolution)

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ try:
     src_files = glob.glob("src/lokit/**/*.py", recursive=True)
     src_files = [f.replace("\\", "/") for f in src_files if "importers.py" not in f]
 
-    ext_modules = mypycify(
-        src_files,
-        opt_level="3",
-        debug_level="0",
-    )
+    import sys
+    import os
+    if sys.platform == "win32" or os.environ.get("LOKIT_PURE_PYTHON") == "1":
+        ext_modules = []
+    else:
+        ext_modules = mypycify(
+            src_files,
+            opt_level="3",
+            debug_level="0",
+        )
 except ImportError:
     ext_modules = []
 
